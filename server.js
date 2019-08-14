3db00e0f2b045e970de5257d9d6560c8c372e385
KimHyeshin <hyeshin2530@gmail.com>
2019-08-15 01:05:14 +0900

    ajax 수정 fs 모듈 추가 router 삭제

--- server.js
+++ server.js
@@ -3,20 +3,74 @@
  */
 var express = require('express');
 var app = express();
-
-/**
- * router
- */
-app.use('/', require('./routes/index'));
-app.use('/list', require('./routes/index'));
-
 var server = app.listen(3000, function(){
     console.log("Express server has started on port 3000")
 })
-var bodyParser = require('body-parser');
 
-// app.set('views', __dirname + '/views');
+// db
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const adapter = new FileSync('db.json');
+const db = low(adapter);
+
+// fs
+var fs = require('fs');
+// ejs
+var ejs = require('ejs');
+
+// parser
+app.use(express.json());
+app.use(express.urlencoded( {extended : true } ));
+
+
+// template engine
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
-app.use(bodyParser.urlencoded({ extended: true }));
+// route
+app.get('/',function(req, res){
+    console.log("GET")
+    var data = db.get('posts').value()
+    res.render('index.ejs', { data:data })
+});
+
+app.post('/',function(req,res){
+    console.log("POST")
+    
+    var searchOptions = req.body.searchOptions;
+    var searchText = req.body.searchText;
+    console.log("searchOptions : " + searchOptions)
+    console.log("searchText : " + searchText)
+
+    // data 불러오기
+    var data = db.get('posts').value();
+    var result = []
+
+    // data 가공
+    if(searchOptions == 'contains'){
+        for(var i=0;i<data.length;i++){
+            console.log(data[i].keyword)
+            console.log(data[i].keyword.indexOf(searchText))
+            if(data[i].keyword.indexOf(searchText) !== -1){
+                result.push(data[i])
+            }
+        }
+    }else if(searchOptions == 'startsWith'){
+        for(var i=0;i<data.length;i++){
+            if(data[i].keyword.indexOf(searchText) === 0){
+                result.push(data[i])
+            }
+        }
+    }
+
+    // html str 만들기
+    // 1. file read
+    var html_str = fs.readFileSync(__dirname + '/views/list.ejs', 'utf-8');
+
+    // 2. compile
+    var template = ejs.compile(html_str);
+    var html = template({ data:result })
+    // console.log(html);
+    res.json({ data:html })
+});
