9279ec0cbf6901bce4d7f60a161cf110c094fe13
KimHyeshin <hyeshin2530@gmail.com>
2019-08-16 11:53:10 +0900

    영문명/한글명 검색 기능 추가

--- server.js
+++ server.js
@@ -37,11 +37,20 @@ app.get('/',function(req, res){
 
 app.post('/',function(req,res){
     console.log("POST")
-    
+
     var searchOptions = req.body.searchOptions;
     var searchText = req.body.searchText;
     console.log("searchOptions : " + searchOptions)
     console.log("searchText : " + searchText)
+
+    var regExpEng = /[a-zA-Z]/;
+    // var regExpKor = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
+    var key = '';
+    if(regExpEng.test(searchText)){
+        key = 'keywordEng'
+    }else{
+        key = 'keywordKor'
+    }
 
     // data 불러오기
     var data = db.get('posts').value();
@@ -50,15 +59,15 @@ app.post('/',function(req,res){
     // data 가공
     if(searchOptions == 'contains'){
         for(var i=0;i<data.length;i++){
-            console.log(data[i].keyword)
-            console.log(data[i].keyword.indexOf(searchText))
-            if(data[i].keyword.indexOf(searchText) !== -1){
+            console.log(data[i][key])
+            console.log(data[i][key].indexOf(searchText))
+            if(data[i][key].indexOf(searchText) !== -1){
                 result.push(data[i])
             }
         }
     }else if(searchOptions == 'startsWith'){
         for(var i=0;i<data.length;i++){
-            if(data[i].keyword.indexOf(searchText) === 0){
+            if(data[i][key].indexOf(searchText) === 0){
                 result.push(data[i])
             }
         }
